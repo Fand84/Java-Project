7f31c2c8f53dbec855a7869910ac492e153cbf02
Fulop Viorica Andreea <66545773+Fand84@users.noreply.github.com>
2020-10-01 20:38:36 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,159 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        /*
+        System.out.println("Hello World");
+
+        int a = 2;
+        int b = 5;
+        int suma;
+
+        suma = 0;
+        suma = a + b;
+
+        System.out.println(" Suma este: " + suma);
+
+        /*
+
+        float x = 2.35f;
+        double z = 4.51543678;
+
+        //char e = 'x';
+
+        System.out.println("Valoarea lui x este: "+ x);
+
+        Integer i = 5;
+        String str ="text";
+        System.out.println(str);
+
+        str = str.concat("+ new text");
+        System.out.println(str);
+        //operatii matematice folosite operatori
+        //+,-, :,
+
+
+         */
+
+        /*
+        a = b;
+        double x = 5.0;
+        System.out.println(a == b);
+        System.out.println(a / 2);
+        System.out.println(x / 2);
+        a+=1;
+        //a=a+1;
+        System.out.println(a!=b);
+
+         */
+
+         /* Scanner scan = new Scanner (System.in);
+
+         System.out.println("Introdu numele tau: ");
+
+         String x = scan.nextLine();
+
+         System.out.println("Salut: " +x+  "!");
+
+          */
+
+         /* System.out.println("Perfect! Introdu primul numar: ");
+
+         int a =scan.nextInt();
+
+         System.out.println("Bun, acum introdu al doilea numar : ");
+
+         int b = scan.nextInt();
+
+         int suma = a+b;
+
+         System.out.println("Suma numerele introduse este : "+ suma);
+
+          */
+
+         /* Scanner scan = new Scanner (System.in);
+
+         System.out.println("Introdu prenumele: ");
+
+         String x = scan.nextLine(  );
+
+         System.out.println("Introdu numele: "+ "  ");
+
+         String y = scan.nextLine(   );
+
+         String nume = x + y;
+
+         System.out.println(" Numele complet este :  "+ nume );
+
+          */
+             // Defineste numele a 3 persoane
+
+             String Pers1 = "Alex";
+             String Pers2 = "Adi";
+             String Pers3 = "Mihai";
+
+
+             //Defineste varsta lor
+
+               int varsta1 = 27;
+               int varsta2 = 32;
+               int varsta3 = 21;
+
+
+               //Defineste hobby-urile lor;
+
+               String hobby1 = "Basket";
+               String hobby2 = "Fotbal";
+               String hobby3 = "Fotografie";
+
+               //Compara varsta pers 1 cu 3 si 2 cu 3
+
+               /* System.out.println("Este egala varsta pers 1 vs pers 3? " + (varsta1 == varsta3));
+               System.out.println("Este egala varsta pers 2 vs pers 3? " + (varsta2 == varsta3));
+
+               //Calculeaza media lor de varsta
+
+               float medieVarsta = (varsta1 + varsta2+ varsta3) / 3;
+
+               System.out.println("Media de varsta in companie este: " + medieVarsta);
+
+               //Afisati pe ecran informatiile despre fiecare in parte
+
+               System.out.println("Persoana 1 se numeste " + Pers1 + ", are o varsta de " +  varsta1  + " ani si este " +
+                       "pasionata de  " + hobby1);
+               System.out.println("Persoana 2 se numeste " + Pers2 + ", are o varsta de " + varsta2 + " ani si este " +
+                       "pasionata de  " + hobby2);
+               System.out.println("Persoana 3 se numeste " + Pers3 + ", are o varsta de " + varsta3 + " ani si este " +
+                "pasionata de  " + hobby3);
+
+
+                /*
+
+                if (varsta1 == varsta3) {
+                    System.out.println(Pers1+ " Nu are aceeasi varsta cu " + Pers3);
+                }
+                else if (varsta1==varsta2) {
+                    System.out.println(Pers1 + " are aceeasi varsta cu " + Pers2);
+                }
+                else if (varsta2>=varsta3) {
+                    System.out.println("Da este mai mare");
+                }
+                    else {
+                    System.out.println("Nu am altceva de afisat");
+                   /*
+
+                   int nota = 8
+
+        }
+    }
+
+
+
+
+
+
+}
